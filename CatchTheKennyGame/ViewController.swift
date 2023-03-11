b586c7fb81d9cce2e242b4c0e2049683c85b9747
Oguzhnblt <oguzhanbolat0@gmail.com>
2023-03-11 16:29:30 +0300

    Initial Commit

---
+++ CatchTheKennyGame/ViewController.swift
@@ -0,0 +1,35 @@
+//
+//  ViewController.swift
+//  CatchTheKennyGame
+//
+//  Created by Oğuz on 11.03.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    @IBOutlet weak var timeLabel: UILabel!
+    @IBOutlet weak var scoreLabel: UILabel!
+    @IBOutlet weak var highScoreLabel: UILabel!
+    
+    @IBOutlet weak var kenny1: UIImageView!
+    @IBOutlet weak var kenny2: UIImageView!
+    @IBOutlet weak var kenny3: UIImageView!
+    @IBOutlet weak var kenny4: UIImageView!
+    @IBOutlet weak var kenny5: UIImageView!
+    @IBOutlet weak var kenny6: UIImageView!
+    @IBOutlet weak var kenny7: UIImageView!
+    @IBOutlet weak var kenny8: UIImageView!
+    @IBOutlet weak var kenny9: UIImageView!
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+    }
+
+    
+
+}
+
